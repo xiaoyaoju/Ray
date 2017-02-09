7a75575447d15e6ab9841ec2beddc595d5524ba3
Kensuke Matsuzaki <knsk.mtzk@gmail.com>
2017-02-09 19:49:32 +0900

    Eval all enqueued nodes if reuse-subtree

--- src/UctSearch.cpp
+++ src/UctSearch.cpp
@@ -2249,9 +2249,11 @@ static std::vector<float> eval_input_data;
 void EvalNode() {
 #if 1
   while (true) {
-    if (handle[0] == nullptr)
-      break;
     LOCK_EXPAND;
+    if (handle[0] == nullptr && ((!reuse_subtree && !ponder) || eval_queue.empty())) {
+      UNLOCK_EXPAND;
+      break;
+    }
 
     if (eval_queue.empty()) {
       UNLOCK_EXPAND;
