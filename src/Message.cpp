f46b17626dd3be374144fe03aaf350b44c2aa824
Kensuke Matsuzaki <knsk.mtzk@gmail.com>
2017-06-07 21:08:21 +0900

    Change stone display to O and X

--- src/Message.cpp
+++ src/Message.cpp
@@ -40,7 +40,7 @@ GetDebugMessageMode()
 void
 PrintBoard( const game_info_t *game )
 {
-  const char stone[S_MAX] = { '+', 'B', 'W', '#' };
+  const char stone[S_MAX] = { '.', 'X', 'O', '#' };
 
   if (!debug_message) return;
 
