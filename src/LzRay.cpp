f2f5f683e5f5c5fc7cb6e9dbeac17b09cc202b0b
Kensuke Matsuzaki <knsk.mtzk@gmail.com>
2019-12-08 12:28:25 +0900

    add boost python

---
+++ src/LzRay.cpp
@@ -0,0 +1,81 @@
+#include <cstring>
+#include <cstdio>
+#if defined (_WIN32)
+#include <windows.h>
+#endif
+
+#include "Command.h"
+#include "GoBoard.h"
+#include "Gtp.h"
+#include "OpeningBook.h"
+#include "PatternHash.h"
+#include "Rating.h"
+#include "Semeai.h"
+#include "Train.h"
+#include "UctRating.h"
+#include "UctSearch.h"
+#include "ZobristHash.h"
+
+
+int
+main( int argc, char **argv )
+{
+  char program_path[1024];
+  int last;
+
+  // 実行ファイルのあるディレクトリのパスを抽出
+#if defined (_WIN32)
+  HMODULE hModule = GetModuleHandle(NULL);
+  GetModuleFileNameA(hModule, program_path, 1024);
+#else
+  strcpy(program_path, argv[0]);
+#endif
+  last = (int)strlen(program_path);
+  while (last--){
+#if defined (_WIN32)
+    if (program_path[last] == '\\' || program_path[last] == '/') {
+      program_path[last] = '\0';
+      break;
+    }
+#else
+    if (program_path[last] == '/') {
+      program_path[last] = '\0';
+      break;
+    }
+#endif
+  }
+
+  // 各種パスの設定
+#if defined (_WIN32)
+  sprintf_s(uct_params_path, 1024, "%s\\uct_params", program_path);
+  sprintf_s(po_params_path, 1024, "%s\\sim_params", program_path);
+#else
+  snprintf(uct_params_path, 1024, "%s/uct_params", program_path);
+  snprintf(po_params_path, 1024, "%s/sim_params", program_path);
+#endif
+  // コマンドライン引数の解析  
+  AnalyzeCommand(argc, argv);
+
+  // 各種初期化
+  InitializeConst();
+  InitializeRating();
+  InitializeUctRating();
+  InitializeUctSearch();
+  InitializeSearchSetting();
+  InitializeHash();
+  InitializeUctHash();
+  SetNeighbor();
+
+  switch (GetRunMode()) {
+  case RUN_MODE::GTP:
+    // GTP
+    GTP_main();
+    break;
+  case RUN_MODE::TRAIN:
+    Train();
+    //ThinkOpeningBook();
+    break;
+  }
+
+  return 0;
+}
